ea6abb5f73b1cd9b2434289e8835ea64e2434ab9
Arpit Gupta <argupt@microsoft.com>
2020-07-28 16:15:55 -0700

    Adding reporting controller and swagger integration

---
+++ src/Web.API/IDatasourceManager.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ThreeFourteen.Finnhub.Client.Model;
+
+namespace FinanceHub
+{
+    public interface IDatasourceManager
+    {
+       Task<Quote> GetQuoteAsync(string symbol);
+    }
+}
--- src/Web.API/Startup.cs
+++ src/Web.API/Startup.cs
@@ -10,11 +10,13 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
 
 namespace FinanceHub
 {
     public class Startup
     {
+        // TODO: Change the key and get it from config
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -26,6 +28,30 @@ namespace FinanceHub
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "Finance hub",
+                    Description = "Sample app for gathering financial data",
+                    TermsOfService = new Uri("https://example.com/terms"),
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Hackathon 2020",
+                        Email = string.Empty,
+                        Url = new Uri("https://twitter.com/spboyer"),
+                    },
+                    License = new OpenApiLicense
+                    {
+                        Name = "Use under LICX",
+                        Url = new Uri("https://example.com/license"),
+                    }
+                });
+            });
+
+            //DatasourceManager datasourceManager = new DatasourceManager(finhubbApiKey);
+            services.AddSingleton<IDatasourceManager, DatasourceManager>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -40,8 +66,19 @@ namespace FinanceHub
                 app.UseHsts();
             }
 
+            
+
             app.UseHttpsRedirection();
             app.UseMvc();
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Finance API V1");
+
+            });
         }
     }
 }
